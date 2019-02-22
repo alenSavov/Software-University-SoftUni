2bafaa7f0bd1ddb0cedf23050dc284c96de1f39f
alenSavov <playmatte@gmail.com>
2019-02-22 23:36:14 +0200

    Added - 01.Stacks and Queues - Lab

---
+++ 04.C#Fundamentals(3.0)/01.C# Advanced/01.Stacks and Queues - Lab/6.MathPotato/MathPotato.cs
@@ -0,0 +1,27 @@
+﻿namespace _6.MathPotato
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    namespace _01._Reverse_Numbers
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var numbers = Console.ReadLine()
+                    .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                var stack = new Stack<string>();
+
+                foreach (var number in numbers)
+                {
+                    stack.Push(number);
+                }
+
+                Console.WriteLine(string.Join(" ", stack));
+            }
+        }
+    }
+}
