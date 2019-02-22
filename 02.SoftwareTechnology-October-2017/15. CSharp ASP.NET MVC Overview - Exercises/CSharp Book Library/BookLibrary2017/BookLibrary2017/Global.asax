d4a511589abe0af3cd51e5086f8cac5d6106b08d
alenSavov <playmatte@gmail.com>
2019-02-22 22:43:40 +0200

    Added - 15. CSharp ASP.NET MVC Overview - Exercises

---
+++ 02.SoftwareTechnology-October-2017/15. CSharp ASP.NET MVC Overview - Exercises/CSharp Book Library/BookLibrary2017/BookLibrary2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookLibrary2017.MvcApplication" Language="C#" %>
---
+++ 02.SoftwareTechnology-October-2017/15. CSharp ASP.NET MVC Overview - Exercises/CSharp Book Library/BookLibrary2017/BookLibrary2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookLibrary2017.Startup))]
+namespace BookLibrary2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
