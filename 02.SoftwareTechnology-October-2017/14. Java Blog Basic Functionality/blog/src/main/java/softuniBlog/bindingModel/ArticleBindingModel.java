1bd9b9f7cd60956c32b9ec26e268287f2a742a19
alenSavov <playmatte@gmail.com>
2019-02-22 22:41:49 +0200

    Added - 14. Java Blog Basic Functionality

---
+++ 02.SoftwareTechnology-October-2017/14. Java Blog Basic Functionality/blog/src/main/java/softuniBlog/bindingModel/ArticleBindingModel.java
@@ -0,0 +1,38 @@
+package softuniBlog.bindingModel;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import softuniBlog.entity.Article;
+
+import javax.validation.constraints.NotNull;
+
+public class ArticleBindingModel {
+    @NotNull
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @NotNull
+    private String content;
+
+
+}
+
+
